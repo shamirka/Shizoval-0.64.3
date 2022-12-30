671d8dc06b4472d8ec54e0cc11bf9c7e72170e2e
UUID1606 <108305740+UUID1606@users.noreply.github.com>
2022-12-30 16:52:27 +0000

    Update UUID.meta.js

--- release/UUID.meta.js
+++ release/UUID.meta.js
@@ -3,7 +3,7 @@
 // @version      0.64.3
 // @description  Free open-source game cheat for Tanki Online.
 // @author       Zorro
-// @match        https://*.tankionline.com/*
+// @match        https://*.test-eu.tankionline.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tankionline.com
     
 // @require      https://raw.githubusercontent.com/flyover/imgui-js/master/dist/imgui.umd.js
