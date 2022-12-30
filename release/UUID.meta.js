53cf0cabd1718417a9d601d91373a0c03bde4820
UUID1606 <108305740+UUID1606@users.noreply.github.com>
2022-12-30 16:14:41 +0000

    Update UUID.meta.js

--- release/UUID.meta.js
+++ release/UUID.meta.js
@@ -1,16 +1,16 @@
 // ==UserScript==
-// @name         TOHBA
+// @name         xZorro Shizoval
 // @version      0.64.3
 // @description  Free open-source game cheat for Tanki Online.
-// @author       sheezzmee
+// @author       Zorro
 // @match        https://*.tankionline.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tankionline.com
     
 // @require      https://raw.githubusercontent.com/flyover/imgui-js/master/dist/imgui.umd.js
 // @require      https://raw.githubusercontent.com/flyover/imgui-js/master/dist/imgui_impl.umd.js
     
-// @downloadURL  https://raw.githubusercontent.com/T0HBA/TOHBA/main/release/TOHBA.user.js
-// @updateURL    https://raw.githubusercontent.com/T0HBA/TOHBA/main/release/TOHBA.meta.js
+// @downloadURL  https://raw.githubusercontent.com/UUID1606/UUID1606/main/release/UUID.user.js
+// @updateURL    https://raw.githubusercontent.com/UUID1606/UUID1606/main/release/UUID.meta.js
     
 // @run-at       document-start
 // @grant        GM_xmlhttpRequest
